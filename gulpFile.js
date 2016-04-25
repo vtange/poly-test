f52f6487b019b2f7cbd479447ca531cab94ab596
Victor Tang <victortang1992@gmail.com>
2016-04-24 20:29:14 -0700

    use gulp system

--- gulpFile.js
+++ gulpFile.js
@@ -3,14 +3,14 @@ var vulcanize = require('gulp-vulcanize');
 var crisper = require('gulp-crisper');
 
 gulp.task('vulcanize', function() {
-  return gulp.src('app/elements/elements.html')
+  return gulp.src('index.html')
     .pipe(vulcanize({
       stripComments: true,
       inlineScripts: true,
       inlineCss: true
     }))
     .pipe(crisper())
-    .pipe(gulp.dest('dist/elements'));
+    .pipe(gulp.dest('index.html'));
 });
 
 gulp.task('default', ['vulcanize']);
